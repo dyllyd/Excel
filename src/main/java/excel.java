2bda8d272fece7cf13e02f32468b6dc8f972a129
dyllyd <42504823+dyllyd@users.noreply.github.com>
2021-04-06 13:43:02 -0400

    Initial commit

---
+++ src/main/java/excel.java
@@ -0,0 +1,31 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+
+public class excel {
+
+    public static void main(String[] args) throws IOException{
+        File excelFile = new File("src/Test.xlsx");
+        FileInputStream fis = new FileInputStream(excelFile);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        for (Row row : sheet) {
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                System.out.println(cell.toString() + ";");
+            }
+            System.out.println();
+        }
+        workbook.close();
+        fis.close();
+    }
+}
+
